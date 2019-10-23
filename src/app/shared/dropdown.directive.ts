59bc2aa3edec03feb211be4c4ee300d1ea447829
mixampo <maxim_v_e@hotmail.com>
2019-10-23 15:36:20 +0200

    added enhancement to header

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,12 +1,18 @@
-import {Directive, Host, HostBinding, HostListener} from '@angular/core';
+import {Directive, ElementRef, Host, HostBinding, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
-  @HostBinding('class.open') isOpen = false;
+  // @HostBinding('class.open') isOpen = false;
+  //
+  // @HostListener('click') toggleOpen() {
+  //   this.isOpen = !this.isOpen;
+  // }
 
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  @HostBinding('class.open') isOpen = false;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
+  constructor(private elRef: ElementRef) {}
 }
